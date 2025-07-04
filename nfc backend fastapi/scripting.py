ec066bc28524775c1925abe45b06d11d28ed0be2
DinnerDinner <singhpranjwal@gmail.com>
2025-07-04 16:41:44 -0400

    dd

---
+++ nfc backend fastapi/scripting.py
@@ -0,0 +1,13 @@
+"""
+To start the server local:
+python -m venv env
+env\Scripts\activate
+uvicorn main:app --host 127.0.0.1 --port 8000
+
+To start the ngrok:
+
+
+To start the docker container with db:
+
+
+"""
