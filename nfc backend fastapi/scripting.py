43f121d1f9bc9c4169e923b5796d0cdabd0650aa
DinnerDinner <singhpranjwal@gmail.com>
2025-07-04 16:51:26 -0400

    tst inside bscode

--- nfc backend fastapi/scripting.py
+++ nfc backend fastapi/scripting.py
@@ -5,9 +5,11 @@ env\Scripts\activate
 uvicorn main:app --host 127.0.0.1 --port 8000
 
 To start the ngrok:
-
+cd into C: only
+ngrok https 8000
 
 To start the docker container with db:
-
+you can play it from docker desktop
+and run docker ps in cmd to check if its up
 
 """
