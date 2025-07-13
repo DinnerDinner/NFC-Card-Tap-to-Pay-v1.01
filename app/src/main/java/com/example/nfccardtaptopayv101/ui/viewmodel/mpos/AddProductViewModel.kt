b48e339b54a235114e181e50da062656df21a0e6
DinnerDinner <singhpranjwal@gmail.com>
2025-07-13 11:56:46 -0400

    use navgraph instead of currentstate

--- app/src/main/java/com/example/nfccardtaptopayv101/ui/viewmodel/mpos/AddProductViewModel.kt
+++ app/src/main/java/com/example/nfccardtaptopayv101/ui/viewmodel/mpos/AddProductViewModel.kt
@@ -4,5 +4,5 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 
 class AddProductViewModel(application: Application) : AndroidViewModel(application) {
-    // TODO: Implement product fields, validation, and API calls later
+    // TODO: Implement product fields, validation, and API calls later RAHHHHH
 }
