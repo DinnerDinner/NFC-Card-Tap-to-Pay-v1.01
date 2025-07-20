60adcde6b0baa9f0fade0e72cf4279a26da544e6
DinnerDinner <singhpranjwal@gmail.com>
2025-07-20 18:14:15 -0400

    test

--- nfc_backend_fastapi/scripting.py
+++ nfc_backend_fastapi/scripting.py
@@ -21,7 +21,7 @@ ngrok http --url=promoted-quetzal-visually.ngrok-free.app 8000
 Take the ngrok url and plug into .url() in MainActivity.kt in Android Studio app and reBuild
 
 
-
+######NO NEED FOR DOCKER ANYMORE SINCE SUPABASE INTEGRATION COMPLETE
 To create the Docker PostgreSQL container with persistent volume:
 docker volume create nfcwallet_data
 docker run --name nfc-postgres -e POSTGRES_USER=nfcuser -e POSTGRES_PASSWORD=nfcpass -e POSTGRES_DB=nfcwallet -v nfcwallet_data:/var/lib/postgresql/data -p 5432:5432 -d postgres
